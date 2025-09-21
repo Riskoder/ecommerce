e0e96ef8aab3ec134678d9f7d31911d4e76d7f57
Riskoder <g.vidal1g+1@gmail.com>
2025-09-21 20:18:38 -0300

    Se agrego el componente de CartOption

---
+++ components/CartOption.js
@@ -0,0 +1,23 @@
+import { getTotalProducts } from '../pages/carritoCompras/cartLogic.js';
+
+export const CartOption = () => {
+  const cartContainer = document.createElement('a');
+  cartContainer.href = '#/cart';
+  cartContainer.classList.add('cart-option');
+
+  const updateBadge = () => {
+    const totalItems = getTotalProducts();
+    cartContainer.innerHTML = `
+      <span class="material-symbols-outlined">shopping_cart</span>
+      <span class="cart-badge ${totalItems === 0 ? 'empty' : ''} ${
+      totalItems > 9 ? 'double-digits' : ''
+    }">${totalItems}</span>
+    `;
+  };
+
+  updateBadge();
+
+  setInterval(updateBadge, 300);
+
+  return cartContainer;
+};
