0a784d3117f08bf9bff6d5f3bb1dc2fa2cf9eaa8
Raul <ra.fernandezduocuc.cl.com>
2025-09-22 21:43:37 -0300

    Cambio mayores realizado a Index se añadio el linkeo además se añadio blogs css js además se añadio home.css

--- pages/blogs/Blogs.js
+++ pages/blogs/Blogs.js
@@ -13,9 +13,10 @@ export function Blogs() {
       <div class="noticia">
         <h3>NOTICIA 1</h3>
       </div>
-    <div class="flex">
+      
       <div class="linea">
-        <p>Nuevas tendencias de moda</p>
+        <h4>Nuevas tendencias de moda en mujeres</h4>
+        <br>
         <p>De la pasarela a la calle, pasando por las tendencias más virales. El mejor análisis de las prendas, los accesorios y 
         los looks que más se llevan temporada tras temporada</p>
       </div>
@@ -28,10 +29,65 @@ export function Blogs() {
         height="300px" 
         width="500px">
       </div>
-    </div>
-      <button class="add-btn">Ver más</button>
+      <a href="https://www.vogue.es/moda/tendencias" target="_blank" rel="noopener noreferrer">
+      <button class="add-btn btn-ver-mas">Ver más</button>
+      </a>
   </section>
+
+  <section class= "border">
+
+      <div class="noticia">
+        <h3>NOTICIA 2</h3>
+      </div>
+      
+      <div class="linea">
+        <h4>Nuevas tendencias de moda</h4>
+        <br>
+        <p>Tendencias primavera / verano 2025: así vestirán los hombres que más saben de moda</p>
+      </div>
+
+      <div class="pct">
+        <img 
+        src="assets/hombres.jpg" 
+        alt="imagen de moda" 
+        class="picture" 
+        height="300px" 
+        width="500px">
+      </div>
+      <a href="https://www.revistagq.com/articulo/tendencias-primavera-verano-2025-hombre-como-vestir" target="_blank" rel="noopener noreferrer">
+      <button class="add-btn btn-ver-mas">Ver más</button>
+      </a>
+  </section>
+
+  <section class= "border">
+
+      <div class="noticia">
+        <h3>NOTICIA 3</h3>
+      </div>
+      
+      <div class="linea">
+        <h4>Nuevas tendencias de relojes</h4>
+        <br>
+        <p>Estas son las tendencias de Watches and Wonders 2025</p>
+      </div>
+
+      <div class="pct">
+        <img 
+        src="assets/reloj.jpg" 
+        alt="imagen de moda" 
+        class="picture" 
+        height="300px" 
+        width="500px">
+      </div>
+      <a href="https://www.chrono24.cl/magazine/estas-son-las-tendencias-de-watches-and-wonders-2025-p_152256/" target="_blank" rel="noopener noreferrer">
+      <button class="add-btn btn-ver-mas">Ver más</button>
+      </a>
+  </section>
+
+
   `;
 
   return container;
 }
+
+
--- pages/home/Home.js
+++ pages/home/Home.js
@@ -3,9 +3,24 @@ export function Home() {
   container.classList.add('container');
 
   container.innerHTML = `
-    <h1>
-      Home Test
-    </h1>
+  <section class= "border-home">
+      
+      <div class="linea-home">
+        <p>Creando nuevas tendencias</p>
+      </div>
+
+      <div class="pct">
+        <img 
+        src="assets/HEROIMAGE.png" 
+        alt="imagen del home" 
+        class="picture-home" 
+        height="300px" 
+        width="600px">
+      </div>
+      <a href="http://127.0.0.1:3000/ecommerce/index.html?serverWindowId=f7a6e821-841a-4fe6-927f-74b7c1aa9633#/products" "rel="noopener noreferrer">
+      <button class="add-btn btn-ver-productos">Ver productos</button>
+      </a>
+  </section>
   `;
 
   return container;
